774233180923b067145369aa0783133b2eb50e3a
odinaka-dev <uchechukwupeter22@gmail.com>
2024-06-17 22:02:56 +0100

    Update apifile.js

--- apifile.js
+++ apifile.js
@@ -2,20 +2,6 @@
 // const apikEY = 'https://nigerian-universities.onrender.com/';
 
 
-const getSchools = async ()=>{
-    // the base school
-    const baseSchools =  `https://nigerian-universities.onrender.com/`;
-   
-    const response = await fetch(baseSchools);
-    const data = await response.json();
-    console.log(data[0]);
-
-
-};
-
-getSchools();
-
-
 
 
 
