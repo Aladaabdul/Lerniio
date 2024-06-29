b64c57ced3f616f4c33ad7df89f4fb7c13249a6d
Zida01 <olaty21bisi@gmail.com>
2024-06-29 12:16:49 +0100

    cookies and cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const PORT = process.env.PORT || 8000
 const cookieParser = require('cookie-parser');
+const cors = require('cors')
 const { connectTomongo } = require("./db")
 const userRouter = require("./routes/user-routes")
 const schoolRouter = require("./routes/school-routes")
@@ -8,15 +9,16 @@ const schoolRouter = require("./routes/school-routes")
 const app = express()
 
 connectTomongo();
+app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+//     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     next();
+// });
 
 app.use("/v1/api/user", userRouter);
 app.use("/v1/api/school", schoolRouter);
